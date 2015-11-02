dc7bc0a333b5c62eaffc1ee0db4397a511496b20
Ted Pyne <ted.pyne.4@gmail.com>
2015-11-02 14:02:27 -0500

    Basic setup

--- src/ChemHelper.java
+++ src/ChemHelper.java
@@ -1,4 +1,18 @@
 
-public class ChemHelper {		//Primary GUI class
+import java.awt.*;
 
+import javax.swing.*;
+
+
+public class ChemHelper extends JFrame{		//Primary GUI class
+	Container pane;
+	
+	public ChemHelper(){
+		pane = getContentPane();
+		pane.setLayout(new BorderLayout());
+	}
+	
+	public static void main(String[] args){
+		new ChemHelper();
+	}
 }
