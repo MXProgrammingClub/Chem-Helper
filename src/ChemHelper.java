f4b0c857455b42ab81eec54c5ccc483a54cfa6c2
Ted Pyne <ted.pyne.4@gmail.com>
2015-11-03 19:19:06 -0500

    More inheitance and GUI

--- src/ChemHelper.java
+++ src/ChemHelper.java
@@ -6,12 +6,30 @@ import javax.swing.*;
 
 public class ChemHelper extends JFrame{		//Primary GUI class
 	Container pane;
+	JList funcs;
 	
 	public ChemHelper(){
 		pane = getContentPane();
 		pane.setLayout(new BorderLayout());
+		
+		
+		funcs = new JList(populateFuncs());
+		
+		
+		pane.add(funcs, BorderLayout.WEST);
+		funcs.setSize(200, HEIGHT);
+		
+		setSize(800,800);
+		setVisible(true);
 	}
 	
+	private Function[] populateFuncs() {
+		Function[] funcs = new Function[1];
+		
+		//funcs[0] = new Function();
+		return funcs;
+	}
+
 	public static void main(String[] args){
 		new ChemHelper();
 	}
---
+++ src/Function.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+
+public abstract class Function {
+	
+	public abstract JFrame getFrame();		//Return the frame containing all components for that chem function
+	
+}
