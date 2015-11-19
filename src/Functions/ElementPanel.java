b3b5fa598c1b5a9acd7b1e2e02da1ef6ff8a44a1
giacalone99 <Luke@Lukes-MacBook-Pro-3.local>
2015-11-18 21:29:24 -0500

    Revert "Revert "Made the  periodic table different""
    This reverts commit cd0982549189edad00fd944b8c31dfdd4bc79030.

--- src/Functions/ElementPanel.java
+++ src/Functions/ElementPanel.java
@@ -33,8 +33,24 @@ public class ElementPanel extends JPanel
 				name += "**";
 			}
 			
-			add(new JLabel(name));
+			this.setLayout(new GridBagLayout());
+			GridBagConstraints c = new GridBagConstraints();
+			c.ipady = -5;
+			c.gridy = 0;
+			add(new JLabel("" + element.getNum()), c);
+
+			c.gridy = 1;
+			add(new JLabel("<html><h3>" + name + "</h3></html>"), c);
+			
+			c.gridy = 2;
+			c.ipady = 0;
+			add(new JLabel("<html><font size=\"1\">" + element.getName() + "</font></html>"), c);
+			
+			setBorder(new CompoundBorder(new LineBorder(Color.BLACK, 1), new EmptyBorder(3, 3, 3, 3)));
+			
+			/*add(new JLabel("<html>" + element.getNum() + "<br><center><h1>" + name + "</h1><br>" + element.getName() + "</center></html>"));
 			setBorder(new CompoundBorder(new LineBorder(Color.BLACK, 1), new EmptyBorder(6, 6, 6, 6)));
+			setSize(50, 50);*/
 		}
 	}
 	
