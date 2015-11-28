a034c3fcc4714182b635dcc697f806084f9ef305
giacalone99 <Luke@lukes-mbp-3.home>
2015-11-28 12:26:04 -0500

    Balancing sort of works
    I wrote a balance thing in equations. I haven’t tested it thoroughly.
    Sometimes it doesn’t simplify all the way but I’ll fix it.

--- src/ChemHelper/Equation.java
+++ src/ChemHelper/Equation.java
@@ -7,6 +7,8 @@ import Functions.Function;
 
 public class Equation
 {
+	
+	private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
 	private ArrayList<Compound> left, right;
 	
 	public Equation()
@@ -76,7 +78,299 @@ public class Equation
 		return right;
 	}
 	
-	public boolean balance()
+	public boolean balance() {
+		if(isBalanced()) return true;
+		String[] equations = createEquations();
+		int[] coefficients = solveEquations(equations);
+		if(coefficients[0] == -1) return false; //if the system could not be solved
+		
+		int index = 0;
+		for(Compound c: left) {
+			c.setNum(coefficients[index]);
+			index++;
+		}
+		for(Compound c: right) {
+			c.setNum(coefficients[index]);
+			index++;
+		}
+		return true;
+	}
+	
+	//creates equations for each ion
+	private String[] createEquations() {
+		String skeleton = "";
+		ArrayList<Ions> ions = new ArrayList<Ions>();
+		int var = 1;
+		for(Compound c: left) { //getting and assigning vars to left side compounds
+			for(Ions e: c.getIons()) //getting all the different ions in the equation
+				if(!ions.contains(e)) ions.add(e);
+			if(!skeleton.equals("")) skeleton += "+";
+			skeleton += getNextVar(var);
+			var++;
+		}
+		skeleton += "=";
+		for(Compound c: right) { //getting and assigning vars to right side compounds
+			if(skeleton.charAt(skeleton.length() - 1) != '=') skeleton += "+";
+			skeleton += getNextVar(var);
+			var++;
+		}
+		
+		String[] equations = new String[ions.size()];
+		int index = 0;
+		//goes and adds coefficients to the equations when needed. Vars w/o coefficients have a coefficient of 0.
+		for(Ions e: ions) {
+			int var2 = 1;
+			String eq = skeleton;
+			for(Compound c: left) {
+				for(Ions i: c.getIons()) {
+					if(i.getElement().toString().equals(e.getElement().toString())) {
+						eq = eq.substring(0, eq.indexOf(getNextVar(var2))) + i.getNum() + eq.substring(eq.indexOf(getNextVar(var2)));
+						break;
+					}
+				}
+				var2++;
+			}
+			for(Compound c: right) {
+				for(Ions i: c.getIons()) {
+					if(i.getElement().toString().equals(e.getElement().toString())) {
+						eq = eq.substring(0, eq.indexOf(getNextVar(var2))) + i.getNum() + eq.substring(eq.indexOf(getNextVar(var2)));
+						break;
+					}
+				}
+				var2++;
+			}
+			equations[index] = eq;
+			index++;
+		}
+		
+		//now need to clean up the equations
+		for(int i = 0; i < equations.length; i++) {
+			String eq = equations[i];
+			for(int j = eq.length() - 1; j > 0; j--) { //gets rid of useless vars everywhere except index 0
+				if((eq.charAt(j) == '+') || (Character.isLetter(eq.charAt(j)) && eq.charAt(j - 1) == '+')) {
+					eq = eq.substring(0, j) + eq.substring(j + 1);
+					j++;
+				}
+			}
+			while(Character.isLetter(eq.charAt(0)) || eq.charAt(0) == '+') //gets rid of useless vars at the beginning
+				eq = eq.substring(1);
+			
+			equations[i] = eq;
+		}
+		
+		return equations;
+	}
+	
+	//provides a new variable
+	//BUG: the variables past the alphabet dont work right  
+	private static String getNextVar(int prev) {
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+		for(; prev > 26; prev -= 26) 
+			nums.add(prev % 26);
+		nums.add(prev);
+		String var = "";
+		for(int i = nums.size() - 1; i >= 0; i--)
+			var += alphabet.charAt(nums.get(i) - 1);
+		return var;
+	}
+	
+	//@FIX make more efficient
+	//returns the number of the variable
+	private static int getVarNum(String var) {
+		int i = 1;
+		while(!getNextVar(i).equals(var))
+			i++;
+		return i;
+	}
+	
+	//attempt at getting the system of equations to solve
+	private int[] solveEquations(String[] equations) {
+		double[] coefficients = new double[left.size() + right.size()];
+		for(int i = 1; i < coefficients.length; i++) //assign -1 to all but the first slots
+			coefficients[i] = -1;
+		coefficients[0] = 1; //we assert that the coefficient of a = 1;
+		
+		boolean done = false;
+
+		while(!done) {
+			for(String eq: equations) {
+				ArrayList<String> vars = new ArrayList<String>(); 
+				int i = 0;
+				while(i < eq.length()) { //to find all the variables in the equation
+					if(!Character.isLetter(eq.charAt(i)))
+						i++;
+					else {
+						String temp2 = "";
+						temp2 += eq.charAt(i);
+						i++;
+						while(i < eq.length() && Character.isLetter(eq.charAt(i))) { //goes to find the entire var in case >1 letter
+							temp2 += eq.charAt(i);
+							i++;
+						}
+						vars.add(temp2);
+					}
+				}
+
+				boolean solvable = false;
+				for(String str: vars) { //sees if only one variable is unknown
+					if(coefficients[getVarNum(str) - 1] == -1)
+						if(!solvable)
+							solvable = true;
+						else //if more than one unknown
+							solvable = false;
+				}
+
+				if(solvable) { //if can solve (ie only one unknown)
+					String temp = eq;
+					int index = 0;
+					while(index < temp.length()) { //goes through each known variable and changes it to "*" + value
+						if(Character.isLetter(temp.charAt(index))) {
+							int start = index;
+							int end;
+							while(index < temp.length() && Character.isLetter(temp.charAt(index)))
+								index++;
+							end = index;
+							if(coefficients[getVarNum(temp.substring(start, end)) - 1] != -1) {
+								temp = temp.substring(0, start) + "*" + coefficients[getVarNum(temp.substring(start, end)) - 1] + temp.substring(end);
+							}
+						}
+						else index++;
+					}
+
+					index = 0;
+					while(index < temp.length()) { //takes all the "*" + value and turns it into a number
+						if(Character.isDigit(temp.charAt(index))) {
+							int start = index;
+							int end;
+							while(Character.isDigit(temp.charAt(index)))
+								index++;
+							end = index;
+							if(temp.charAt(end) == '*') {
+								int start2 = end + 1;
+								int end2 = end + 2;
+								while(end2 < temp.length() && (Character.isDigit(temp.charAt(end2)) || temp.charAt(end2) == '.')) //finds the end of the var value
+									end2++;
+								double newVal = Double.parseDouble(temp.substring(start, end)) * Double.parseDouble(temp.substring(start2, end2));
+								temp = temp.substring(0, start) + newVal + temp.substring(end2);
+								index = start + ("" + newVal).length();
+							}
+						}
+						else index++;
+					}
+
+					//to determine the side of the '=' the variable is on
+					boolean isLeft = false; //the side that the variable is on
+					index = 0;
+					while(temp.charAt(index) != '=') {
+						if(Character.isLetter(temp.charAt(index))) {
+							isLeft = true;
+							break;
+						}
+						index++;
+					}
+
+					//to add the numbers on the non var side of the equation (ie 2+5=3c becomes 7+3c)
+					String side;
+					if(!isLeft) 
+						side = temp.split("=")[0];
+					else
+						side = temp.split("=")[1];
+					if(side.indexOf("+") < 0); //if there is no addition to be done
+					else {
+						String[] nums = side.split("+");
+						int total = 0;
+						for(String str: nums)
+							total += Integer.parseInt(str);
+						if(!isLeft)
+							temp = "" + total + temp.substring(temp.indexOf("="));
+						else
+							temp = temp.substring(0, temp.indexOf("=") + 1) + total;
+					}
+
+					//at this point, I have ie 5=2c+3 or 1=2c and need to subtract and divide
+					if(isLeft && temp.substring(0, temp.indexOf("=")).indexOf("+") > 0) {
+						String[] stuff = temp.substring(0, temp.indexOf("=")).split("+");
+						int total = 0;
+						String var = "";
+						for(String str: stuff) {
+							try {
+								total += Integer.parseInt(str);
+							}
+							catch(NumberFormatException e) {
+								var = str;
+							}
+						}
+						temp = "" + (Integer.parseInt(temp.split("=")[0]) - total) + "=" + var;
+					}
+					if(!isLeft && temp.substring(temp.indexOf("=")).indexOf("+") > 0) {
+						String[] stuff = temp.substring(temp.indexOf("=")).split("+");
+						int total = 0;
+						String var = "";
+						for(String str: stuff) {
+							try {
+								total += Integer.parseInt(str);
+							}
+							catch(NumberFormatException e) {
+								var = str;
+							}
+						}
+						temp = var + "=" + (Integer.parseInt(temp.split("=")[1]) - total);
+					}
+
+					//now to divide (ie 4=2a becomes 2=a)
+					if(isLeft) {
+						String var = temp.split("=")[0];
+						String num = "";
+						int j = 0;
+						while(Character.isDigit(var.charAt(j))) {
+							num += var.charAt(j);
+							j++;
+						}
+						var = var.substring(j);
+						//System.out.println(var + ", " + num + ", " + temp);
+						temp = var + "= " + (Double.parseDouble(temp.split("=")[1]) / Double.parseDouble(num));
+					}
+					else {
+						String var = temp.split("=")[1];
+						String num = "";
+						int j = 0;
+						while(Character.isDigit(var.charAt(j))) {
+							num += var.charAt(j);
+							j++;
+						}
+						var = var.substring(j);
+						temp = var + "=" + (Double.parseDouble(temp.split("=")[0]) / Double.parseDouble(num));
+					}
+
+					//storing the coefficient in its place in the coefficients array
+					coefficients[getVarNum(temp.split("=")[0]) - 1] = Double.parseDouble(temp.split("=")[1]);
+				}
+
+			}
+
+			done = true;
+			for(double d: coefficients)
+				if(d == -1) done = false;
+
+		}
+		
+		return integerize(coefficients);
+	}
+	
+	//takes an array of doubles and makes them into integers
+	private static int[] integerize(double[] coefficients) {
+		double denom = 1;
+		for(double d: coefficients)
+			if(d % 1 > 0)
+				denom *= (d % 1);
+		int[] newCo = new int[coefficients.length];
+		for(int i = 0; i < coefficients.length; i++)
+			newCo[i] = (int) (coefficients[i] / denom);
+		
+		return newCo;
+	}
+	
+	/*public boolean balance()
 	{
 		if(isBalanced()) return true;
 		ArrayList<Compound> oldLeft = left, oldRight = right;
@@ -98,12 +392,12 @@ public class Equation
 	{
 		if(num == 1) return balance1();
 		return false;
-	}
+	}*/
 	
 	/*
 	 * Balances the equation if the right side is one compound and the left side compounds all contain exactly one element.
 	 */
-	private boolean balance1()
+	/*private boolean balance1()
 	{
 		//Checks if the conditions are met first before using the method.
 		if(right.size() != 1) return false;
@@ -134,7 +428,7 @@ public class Equation
 			}
 		}
 		return isBalanced();
-	}
+	}*/
 	
 	private boolean isBalanced()
 	{
