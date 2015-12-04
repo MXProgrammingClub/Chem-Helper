fc9e42d127ea0923a08f1c8089baf7eed7a7fbcc
juliamcclellan <jmcclellan@mxschool.edu>
2015-12-03 21:00:29 -0500

    More situations for effusion
    I still have to do all the ones with rates.

--- src/Functions/Effusion.java
+++ src/Functions/Effusion.java
@@ -4,9 +4,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Box;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 import ChemHelper.Compound;
@@ -14,9 +16,10 @@ import ChemHelper.InvalidInputException;
 
 public class Effusion extends Function 
 {
-	private JPanel panel;
-	private JLabel instruction, result;
-	private JTextField comp1, comp2;
+	private JPanel panel, c1Panel, c2Panel, ratePanel;
+	private JLabel instruction, result, c1Label, c2Label, rateLabel;
+	private JTextField comp1, comp2, ratio;
+	private JRadioButton formula1, formula2, mass1, mass2;
 	private JButton calculate;
 	private Box box;
 	
@@ -24,9 +27,40 @@ public class Effusion extends Function
 	{
 		super("Law of Effusion");
 		
-		instruction = new JLabel("Enter the two compounds.");
+		instruction = new JLabel("Enter what you know.");
+		
+		c1Label = new JLabel("Compound 1: ");
 		comp1 = new JTextField(5);
+		formula1 = new JRadioButton("Formula", true);
+		mass1 = new JRadioButton("Molar mass");
+		ButtonGroup g1 = new ButtonGroup();
+		g1.add(formula1);
+		g1.add(mass1);
+		c1Panel = new JPanel();
+		c1Panel.add(c1Label);
+		c1Panel.add(comp1);
+		c1Panel.add(formula1);
+		c1Panel.add(mass1);
+
+		c2Label = new JLabel("Compound 2: ");
 		comp2 = new JTextField(5);
+		formula2 = new JRadioButton("Formula", true);
+		mass2 = new JRadioButton("Molar mass");
+		ButtonGroup g2 = new ButtonGroup();
+		g2.add(formula2);
+		g2.add(mass2);
+		c2Panel = new JPanel();
+		c2Panel.add(c2Label);
+		c2Panel.add(comp2);
+		c2Panel.add(formula2);
+		c2Panel.add(mass2);
+		
+		rateLabel = new JLabel("Ratio of rates: ");
+		ratio = new JTextField(5);
+		ratePanel = new JPanel();
+		ratePanel.add(rateLabel);
+		ratePanel.add(ratio);
+		
 		calculate = new JButton("Calculate");
 		calculate.addActionListener(new Calculate());
 		result = new JLabel();
@@ -34,8 +68,9 @@ public class Effusion extends Function
 		box = Box.createVerticalBox();
 		box.add(instruction);
 		box.add(Box.createVerticalStrut(5));
-		box.add(comp1);
-		box.add(comp2);
+		box.add(c1Panel);
+		box.add(c2Panel);
+		box.add(ratePanel);
 		box.add(Box.createVerticalStrut(5));
 		box.add(calculate);
 		box.add(Box.createVerticalStrut(10));
@@ -51,16 +86,40 @@ public class Effusion extends Function
 		{
 			try
 			{
-				Compound c1 = Compound.parseCompound(comp1.getText());
-				Compound c2 = Compound.parseCompound(comp2.getText());
-				double results = Math.sqrt(c1.getMolarMass() / c2.getMolarMass());
-				result.setText("" + results);
+				if(!comp1.getText().equals("") && !comp2.getText().equals("") && formula1.isSelected() && formula2.isSelected())
+				{
+					Compound c1 = Compound.parseCompound(comp1.getText());
+					Compound c2 = Compound.parseCompound(comp2.getText());
+					double results = Math.sqrt(c1.getMolarMass() / c2.getMolarMass());
+					result.setText("Ratio of rates = " + results);
+				}
+				else if(!comp1.getText().equals("") && !comp2.getText().equals("") && formula1.isSelected() && !formula2.isSelected())
+				{
+					Compound c1 = Compound.parseCompound(comp1.getText());
+					double mass2 = Double.parseDouble(comp2.getText());
+					double results = Math.sqrt(c1.getMolarMass() / mass2);
+					result.setText("Ratio of rates = " + results);
+				}
+				else if(!comp1.getText().equals("") && !comp2.getText().equals("") && !formula1.isSelected() && formula2.isSelected())
+				{
+					double mass1 = Double.parseDouble(comp1.getText());
+					Compound c2 = Compound.parseCompound(comp2.getText());
+					double results = Math.sqrt(mass1 / c2.getMolarMass());
+					result.setText("Ratio of rates = " + results);
+				}
+				else if(!comp1.getText().equals("") && !comp2.getText().equals("") && !formula1.isSelected() && !formula2.isSelected())
+				{
+					double mass1 = Double.parseDouble(comp1.getText());
+					double mass2 = Double.parseDouble(comp2.getText());
+					double results = Math.sqrt(mass1 / mass2);
+					result.setText("Ratio of rates = " + results);
+				}
 			}
 			catch(Throwable e)
 			{
 				if(!(e instanceof InvalidInputException))
 				{
-					e = new InvalidInputException(0);
+					e = new InvalidInputException(-1);
 				}
 				result.setText(((InvalidInputException)e).getMessage());
 			}
