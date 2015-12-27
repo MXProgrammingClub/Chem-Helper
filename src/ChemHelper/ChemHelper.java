2cc981103d8004480028d8b7219e97c4b119786d
juliamcclellan <jmcclellan@mxschool.edu>
2015-12-26 21:41:54 -0500

    Empirical formula
    Calculates the empirical formula of a compound given its percent
    composition, with the option of finding the molecular formula if an
    amount of the compound is given.

--- src/ChemHelper/ChemHelper.java
+++ src/ChemHelper/ChemHelper.java
@@ -62,7 +62,7 @@ public class ChemHelper extends JFrame{		//Primary GUI class
 
 	private void createMenu()
 	{
-		funcs = new Function[8];
+		funcs = new Function[9];
 		funcs[0] = new PeriodicTable();
 		funcs[1] = new ElectronShell();
 		funcs[2] = new Stoichiometry();
@@ -71,8 +71,8 @@ public class ChemHelper extends JFrame{		//Primary GUI class
 		funcs[5] = new EquationReader();
 		funcs[6] = new RateLaw();
 		funcs[7] = new Effusion();
+		funcs[8] = new Empirical();
 		
-		//Currently this system is completely random as there are not enough things to make an actually useful system but I wanted to make the framework
 		String[] menuNames = {"General Information", "Stoichiometry", "Other"}; //Lists the names of the different menus on the menu bar.
 		int[] menuCutoffs = {0, 2, 5}; //Specifies the indices where a new menu would start from funcs
 		
---
+++ src/Functions/Empirical.java
@@ -0,0 +1,262 @@
+package Functions;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import Elements.*;
+
+public class Empirical extends Function
+{
+	private static final double ERROR = 101, REST = -1, TOLERANCE = .01;
+	private static final String ENTER_REST = "R";
+	private static final int MAX_TEST = 20;
+	
+	private JPanel panel, amounts;
+	private JTextField moles, mass;
+	private ArrayList<TableRow> rows;
+	private Box rowBox;
+	private JButton calculate, addRow;
+	private JLabel error, empirical, molecular;
+	
+	public Empirical()
+	{
+		super("Empirical Formula");
+		
+		JLabel element = new JLabel(" Element  ");
+		element.setBorder(BorderFactory.createLineBorder(Color.black));
+		JLabel percent = new JLabel(" Percent  ");
+		percent.setBorder(BorderFactory.createLineBorder(Color.black));
+		JPanel topRow = new JPanel();
+		topRow.add(element);
+		topRow.add(percent);
+		
+		rowBox = Box.createVerticalBox();
+		rowBox.add(topRow);
+		rows = new ArrayList<TableRow>();
+		for(int num = 0; num < 2; num++)
+		{
+			TableRow row = new TableRow();
+			rowBox.add(row);
+			rows.add(row);
+		}
+		
+		addRow = new JButton("Add row");
+		addRow.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent arg0)
+					{
+						panel.setVisible(false);
+						TableRow row = new TableRow();
+						rowBox.add(row);
+						rows.add(row);
+						panel.setVisible(true);
+					}
+				});
+		addRow.setAlignmentX(Component.LEFT_ALIGNMENT);
+		
+		calculate = new JButton("Calculate");
+		calculate.setAlignmentX(Component.LEFT_ALIGNMENT);
+		calculate.addActionListener(new Calculate());
+		
+		error = new JLabel();
+		empirical = new JLabel();
+		molecular = new JLabel();
+		
+		moles = new JTextField(5);
+		mass = new JTextField(5);
+		amounts = new JPanel();
+		amounts.add(new JLabel("Moles: "));
+		amounts.add(moles);
+		amounts.add(new JLabel("  Mass: "));
+		amounts.add(mass);
+		
+		Box box = Box.createVerticalBox();
+		box.add(new JLabel("If one percent is given as the remainder, enter \"R\""));
+		box.add(rowBox);
+		box.add(addRow);
+		box.add(Box.createVerticalStrut(10));
+		box.add(new JLabel("If known, enter the "));
+		box.add(amounts);
+		box.add(Box.createVerticalStrut(10));
+		box.add(calculate);
+		box.add(error);
+		box.add(empirical);
+		box.add(molecular);
+		
+		panel = new JPanel();
+		panel.add(box);
+	}
+	
+	private class TableRow extends JPanel
+	{
+		private JTextField element, percent;
+		
+		public TableRow()
+		{
+			element = new JTextField(5);
+			element.setBorder(BorderFactory.createLineBorder(Color.black));
+			percent = new JTextField(5);
+			percent.setBorder(BorderFactory.createLineBorder(Color.black));
+			add(element);
+			add(percent);
+		}
+		
+		public Element getElement()
+		{
+			return PeriodicTable.find(element.getText());
+		}
+		
+		public double getPercent()
+		{
+			String text = percent.getText();
+			if(text.equals(ENTER_REST)) return REST;
+			try
+			{
+				double percent = Double.parseDouble(text);
+				if(percent >= 100 || percent <= 0) return ERROR;
+				else return percent;
+			}
+			catch(Throwable e)
+			{
+				return ERROR;
+			}
+		}
+	}
+	
+	private class Calculate implements ActionListener
+	{
+		public void actionPerformed(ActionEvent arg0)
+		{
+			double[] values = new double[rows.size()];
+			Element[] elements = new Element[rows.size()];
+			int rest = -1;
+			double sum = 0;
+			for(int index = 0; index < rows.size(); index++)
+			{
+				values[index] = rows.get(index).getPercent();
+				elements[index] = rows.get(index).getElement();
+				if(values[index] == ERROR)
+				{
+					error.setText("Value entered outside range of 0 to 100.");
+					return;
+				}
+				if(elements[index] == null)
+				{
+					error.setText("Invalid element entered.");
+					return;
+				}
+				if(values[index] == REST)
+				{
+					if(rest != -1)
+					{
+						error.setText("Only one value can be replaced by \"R\".");
+						return;
+					}
+					rest = index;
+				}
+				else
+				{
+					sum += values[index];
+				}
+			}
+			if(rest != -1)
+			{
+				if(sum >= 100)
+				{
+					error.setText("Your percent values add up to more than 100.");
+					return;
+				}
+				values[rest] = 100 - sum;
+			}
+			else if(Math.abs(100 - sum) > TOLERANCE)
+			{
+				error.setText("Your percent values do not add up to 100.");
+				return;
+			}
+			double min = Double.MAX_VALUE;
+			for(int index = 0; index < values.length; index++)
+			{
+				values[index] /= elements[index].getMolarMass();
+				if(min > values[index]) min = values[index];
+			}
+			int times = 1;
+			for(int index = 0; index < values.length; index++)
+			{
+				values[index] /= min;
+				if(!closeToInt(values[index]))
+				{
+					for(int num = 2; num < MAX_TEST; num++)
+					{
+						double value = num * values[index];
+						if(closeToInt(value))
+						{
+							if(times % num != 0) times *= num;
+							break;
+						}
+					}
+				}
+			}
+			int[] coefficients = new int[values.length];
+			String formula = "<html>Empirical formula: ";
+			double eMass = 0;
+			for(int index = 0; index < coefficients.length; index++)
+			{
+				coefficients[index] = round(values[index] * times);
+				eMass += coefficients[index] * elements[index].getMolarMass();
+				formula += elements[index].getSymbol();
+				if(coefficients[index] != 1) formula += "<sub>" + coefficients[index] + "</sub>";
+			}
+			formula += "     Molar Mass: " + eMass + "</html>";
+			empirical.setText(formula);
+			if(mass.getText() != null)
+			{
+				try
+				{
+					double mMass = Double.parseDouble(mass.getText()) / Double.parseDouble(moles.getText());
+					int factor = round(mMass / eMass);
+					formula = "<html>Molecular formula: ";
+					for(int index = 0; index < coefficients.length; index++)
+					{
+						coefficients[index] *= factor;
+						formula += elements[index].getSymbol();
+						if(coefficients[index] != 1) formula += "<sub>" + coefficients[index] + "</sub>";
+					}
+					formula += "     Molar Mass: " + mMass + "</html>";
+					molecular.setText(formula);
+				}
+				catch(Throwable e)
+				{
+					error.setText("There was a problem with your input.");
+				}
+			}
+		}
+		
+		private boolean closeToInt(double num)
+		{
+			int down = (int)num, up = down + 1;
+			return num - down < TOLERANCE || up - num < TOLERANCE;
+		}
+		
+		private int round(double num)
+		{
+			int round = (int)num;
+			if(round + .5 > num) return round;
+			else return round + 1;
+		}
+	}
+	
+	public JPanel getPanel()
+	{
+		return panel;
+	}
+}
