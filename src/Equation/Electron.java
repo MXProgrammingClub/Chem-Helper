8836435fcfce29dce71d6f75b6f900eeaebc88b9
juliamcclellan <jmcclellan@mxschool.edu>
2016-03-03 22:11:10 -0500

    Electron for electrochemistry
    Not sure if this is going to work.

---
+++ src/Equation/Electron.java
@@ -0,0 +1,51 @@
+/*
+ * Represents an electron.
+ * 
+ * Author: Julia McClellan
+ * Version: 3/3/16
+ */
+
+package Equation;
+
+public class Electron extends Ions
+{
+	public Electron(int num)
+	{
+		super(num, -1);
+	}
+	
+	public boolean equals(Ions other)
+	{
+		return other instanceof Electron;
+	}
+	
+	public Monatomic[] getElements()
+	{
+		return null;
+	}
+	
+	public Monatomic[] getElements(boolean numbers)
+	{
+		return null;
+	}
+	
+	public String toString()
+	{
+		return getNum() == 1 ? "" : getNum() + "e<sub>-</sub>";
+	}
+	
+	public double getMolarMass()
+	{
+		return 0;
+	}
+	
+	public String getMolarMassSteps()
+	{
+		return null;
+	}
+	
+	public String withoutCharge()
+	{
+		return toString();
+	}
+}
