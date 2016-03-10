893d37cc756ff4035a0b261870ec38be5d4fdc72
juliamcclellan <jmcclellan@mxschool.edu>
2016-03-10 12:31:28 -0500

    Wrong symbol for Vanadium

--- src/Elements/Vanadium.java
+++ src/Elements/Vanadium.java
@@ -4,6 +4,6 @@ public class Vanadium extends Element
 {
 	public Vanadium()
 	{
-		super(23, 5, 4, "Vanadium", "Va", 50.9415, 3680, 2183, 6);
+		super(23, 5, 4, "Vanadium", "V", 50.9415, 3680, 2183, 6);
 	}
 }
