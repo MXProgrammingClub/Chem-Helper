5d75987291210e1e88dbd77ce61ce711f651808c
juliamcclellan <jmcclellan@mxschool.edu>
2016-03-11 12:21:22 -0500

    Gibbs energy spontaneous temperature range

--- src/Functions/GibbsEnergy.java
+++ src/Functions/GibbsEnergy.java
@@ -3,8 +3,9 @@
  * number returns true- saves last calculated value and can use saved in any field.
  * 
  * Author: Julia McClellan
- * Version: 2/6/2016
+ * Version: 3/11/2016
  */
+
 package Functions;
 
 import java.awt.event.ActionEvent;
@@ -46,40 +47,86 @@ public class GibbsEnergy extends Function
 					public void actionPerformed(ActionEvent arg0)
 					{
 						steps.removeAll();
+						answer.setText("");
 						double dH = fields[0].getAmount(), t = fields[1].getAmount(), dS = fields[2].getAmount();
-						if(dH == Units.ERROR_VALUE || dH == Units.UNKNOWN_VALUE || t == Units.UNKNOWN_VALUE || t == Units.ERROR_VALUE 
+						if(dH == Units.ERROR_VALUE || dH == Units.UNKNOWN_VALUE || t == Units.ERROR_VALUE 
 								|| dS == Units.ERROR_VALUE || dS == Units.UNKNOWN_VALUE)
 						{
 							answer.setText("There was a problem with your input.");
 							spontaneous.setText("");
 							return;
 						}
-						int sigFigs = Math.min(Math.min(fields[0].getSigFigs(), fields[1].getSigFigs()), fields[2].getSigFigs());
-						dG = dH - t * dS;
+						int sigFigs = Math.min(fields[0].getSigFigs(), fields[2].getSigFigs());
 						
-						steps.add(new JLabel("\u0394G = \u0394H - T\u0394S"));
-						steps.add(Box.createVerticalStrut(5));
-						steps.add(new JLabel("\u0394H = " + dH));
-						steps.add(new JLabel("T = " + t + " K"));
-						steps.add(new JLabel("\0394S = " + dS));
-						steps.add(Box.createVerticalStrut(5));
-						steps.add(new JLabel("\u0394G = " + dH + " - " + t + " * " + dS + " = " + dG));
-						
-						answer.setText("\u0394G = " + Function.withSigFigs(dG, sigFigs));
-						if(dG > 0)
+						if(t == Units.UNKNOWN_VALUE)
 						{
-							spontaneous.setText("Not spontaneous");
-							steps.add(new JLabel(dG + " > 0, so the reaction is not spontaneous"));
-						}
-						else if(dG == 0)
-						{
-							spontaneous.setText("Equilibrium");
-							steps.add(new JLabel(dG + " = 0, so the reaction is at equilibrium"));
+							steps.add(new JLabel("\u0394G = \u0394H - T\u0394S"));
+							steps.add(Box.createVerticalStrut(5));
+							steps.add(new JLabel("\u0394H = " + dH));
+							steps.add(new JLabel("\u0394S = " + dS));
+							steps.add(Box.createVerticalStrut(5));
+							steps.add(new JLabel("For spontaneous reactions, \u0394H - T\u0394S < 0"));
+							steps.add(new JLabel("\u0394H < T\u0394S"));
+							
+							t = dH / dS;
+							if(dS > 0)
+							{
+								steps.add(new JLabel("\u0394H / \u0394S < T"));
+								steps.add(new JLabel(dH + " / " + dS + " < T"));
+								steps.add(new JLabel(t + " K < T"));
+								t = fields[1].getBlankAmount(t);
+								if(t < 0) steps.add(new JLabel("The reaction is always spontaneous."));
+								else
+								{
+									steps.add(new JLabel("Reaction is spontaneous for T > " + t + " " + fields[1].getUnitName()));
+									answer.setText("T > " + Function.withSigFigs(t, sigFigs) + " " + fields[1].getUnitName());
+									dG = t; //For saving the number.
+								}
+							}
+							else
+							{
+								steps.add(new JLabel("\u0394H / \u0394S > T"));
+								steps.add(new JLabel(dH + " / " + dS + " > T"));
+								steps.add(new JLabel(t + " K > T"));
+								t = fields[1].getBlankAmount(t);
+								if(t < 0) steps.add(new JLabel("Impossible temperature range. The reaction is never spontaneous."));
+								else
+								{
+									steps.add(new JLabel("Reaction is spontaneous for T < " + t + " " + fields[1].getUnitName()));
+									answer.setText("T < " + Function.withSigFigs(t, sigFigs) + " " + fields[1].getUnitName());
+									dG = t; //For saving the number.
+								}
+							}
 						}
 						else
 						{
-							spontaneous.setText("Spontaneous");
-							steps.add(new JLabel(dG + " < 0, so the reaction is spontaneous"));
+							sigFigs = Math.min(sigFigs, fields[1].getSigFigs());
+							dG = dH - t * dS;
+						
+							steps.add(new JLabel("\u0394G = \u0394H - T\u0394S"));
+							steps.add(Box.createVerticalStrut(5));
+							steps.add(new JLabel("\u0394H = " + dH));
+							steps.add(new JLabel("T = " + t + " K"));
+							steps.add(new JLabel("\0394S = " + dS));
+							steps.add(Box.createVerticalStrut(5));
+							steps.add(new JLabel("\u0394G = " + dH + " - " + t + " * " + dS + " = " + dG));
+						
+							answer.setText("\u0394G = " + Function.withSigFigs(dG, sigFigs));
+							if(dG > 0)
+							{
+								spontaneous.setText("Not spontaneous");
+								steps.add(new JLabel(dG + " > 0, so the reaction is not spontaneous"));
+							}
+							else if(dG == 0)
+							{
+								spontaneous.setText("Equilibrium");
+								steps.add(new JLabel(dG + " = 0, so the reaction is at equilibrium"));
+							}
+							else
+							{
+								spontaneous.setText("Spontaneous");
+								steps.add(new JLabel(dG + " < 0, so the reaction is spontaneous"));
+							}
 						}
 					}
 				});
