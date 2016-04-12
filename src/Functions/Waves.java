89b991ece06a24e1ef5e9ec849ed594533c0acd3
giacalone99 <giacalone99@gmail.com>
2016-04-12 14:56:24 -0400

    Finished Waves

--- src/Functions/Waves.java
+++ src/Functions/Waves.java
@@ -75,12 +75,13 @@ public class Waves extends Function {
 			steps.removeAll();
 			if(!input[0].isEmpty()) {
 				double newAmount = Units.toBaseUnit(input[0].getAmount(), input[0].getUnit());
-				steps.add(new JLabel("" + input[0].getAmount() + input[0].getUnitName() + " = " + newAmount + "m"));
+				//steps.add(new JLabel("" + input[0].getAmount() + input[0].getUnitName() + " = " + newAmount + "m"));
 				input[0].setAmount(newAmount);
+				
 			}
 			if(!input[3].isEmpty()) {
 				double newAmount = Units.toBaseUnit(input[3].getAmount(), input[3].getUnit());
-				steps.add(new JLabel("" + input[1].getAmount() + input[1].getUnitName() + " = " + newAmount + "g"));
+				//steps.add(new JLabel("" + input[1].getAmount() + input[1].getUnitName() + " = " + newAmount + "g"));
 				input[3].setAmount(newAmount);
 			}
 			steps.add(new JLabel());
@@ -88,68 +89,73 @@ public class Waves extends Function {
 				if(input[0].isEmpty() && !input[1].isEmpty() && !input[5].isEmpty()) { //wavelength
 					double newAmount = input[5].getAmount() / input[1].getAmount();
 					input[0].setAmount(newAmount);
-					steps.add(new JLabel(input[5].getAmount() +  "m/s / " + input[1].getAmount() + "Hz = " + newAmount + "m"));
+					steps.add(new JLabel("c = \u03BB\u22C5f \u2192 " + input[5].getAmount() +  "m/s / " + input[1].getAmount() 
+							+ "Hz = " + newAmount + "m"));
 				}
 				else if(input[0].isEmpty() && !input[3].isEmpty() && !input[3].isEmpty()) { //wavelength
 					double newAmount = input[6].getAmount() / (input[3].getAmount() * input[4].getAmount());
 					input[0].setAmount(newAmount);
-					steps.add(new JLabel(input[6].getAmount() + "J\u00B7s / (" + input[3].getAmount() + "g * "
-							+ input[4].getAmount() + "m/s) =" + newAmount + "m"));
+					steps.add(new JLabel("\u03BB = h/(m\u22C5v) \u2192 " + input[6].getAmount() + "J\u00B7s / (" 
+							+ input[3].getAmount() + "g * " + input[4].getAmount() + "m/s) =" + newAmount + "m"));
 				}
 				else if(input[1].isEmpty() && !input[0].isEmpty() && !input[5].isEmpty()) { //frequency
 					double newAmount = input[5].getAmount() / input[0].getAmount();
 					input[1].setAmount(newAmount);
-					steps.add(new JLabel(input[5].getAmount() + "m/s / " + input[0].getAmount() + "m = " + newAmount + "Hz"));
+					steps.add(new JLabel("c = \u03BB\u22C5f \u2192 " + input[5].getAmount() + "m/s / " + input[0].getAmount() 
+							+ "m = " + newAmount + "Hz"));
 				}
 				else if(input[1].isEmpty() && !input[2].isEmpty() && !input[6].isEmpty()) { //frequency
 					double newAmount = input[2].getAmount() / input[6].getAmount();
 					input[1].setAmount(newAmount);
-					steps.add(new JLabel(input[2].getAmount() + "J / " + input[6].getAmount() + "J\u00B7s = " 
+					steps.add(new JLabel("E = h\u22C5f" + input[2].getAmount() + "J / " + input[6].getAmount() + "J\u00B7s = " 
 							+ newAmount + "Hz"));
 				}
 				else if(input[2].isEmpty() && !input[0].isEmpty() && !input[5].isEmpty()) { //energy
 					double newAmount = input[5].getAmount() / input[0].getAmount();
 					input[2].setAmount(newAmount);
-					steps.add(new JLabel(input[5].getAmount() + "m/s / " + input[0].getAmount() + "m = " + newAmount + "J"));
+					steps.add(new JLabel("E = h\u22C5f" + input[5].getAmount() + "m/s / " + input[0].getAmount() + "m = " 
+							+ newAmount + "J"));
 				}
 				else if(input[3].isEmpty() && !input[0].isEmpty() && !input[4].isEmpty() && !input[6].isEmpty()) { //mass
 					double newAmount = input[6].getAmount() / (input[0].getAmount() * input[4].getAmount());
 					input[3].setAmount(newAmount);
-					steps.add(new JLabel(input[6].getAmount() + "J\u00B7s / (" + input[0].getAmount() + "m * "
-							+ input[4].getAmount() + "m/s) = " + newAmount + "g"));
+					steps.add(new JLabel("\u03BB = h/(m\u22C5v) \u2192 " + input[6].getAmount() + "J\u00B7s / (" 
+							+ input[0].getAmount() + "m * " + input[4].getAmount() + "m/s) = " + newAmount + "g"));
 				}
 				else if(input[4].isEmpty() && !input[0].isEmpty() && !input[3].isEmpty() && !input[6].isEmpty()) { //velocity
 					double newAmount = input[6].getAmount() / (input[0].getAmount() * input[3].getAmount());
 					input[4].setAmount(newAmount);
-					steps.add(new JLabel(input[6].getAmount() + "J\u00B7s / (" + input[0].getAmount() + "m * "
-							+ input[3].getAmount() + "g) = " + newAmount + "m/s"));
+					steps.add(new JLabel("\u03BB = h/(m\u22C5v) \u2192 " + input[6].getAmount() + "J\u00B7s / (" 
+							+ input[0].getAmount() + "m * " + input[3].getAmount() + "g) = " + newAmount + "m/s"));
 				}
 				else if(input[5].isEmpty() && !input[0].isEmpty() && !input[1].isEmpty()) { //speed of light
 					double newAmount = input[0].getAmount() * input[1].getAmount();
 					input[5].setAmount(newAmount);
-					steps.add(new JLabel(input[0].getAmount() + "m * " + input[1].getAmount() + "Hz = " + newAmount + "m/s"));
+					steps.add(new JLabel("c = \u03BB\u22C5f \u2192 " + input[0].getAmount() + "m * " + input[1].getAmount() 
+							+ "Hz = " + newAmount + "m/s"));
 				}
 				else if(input[6].isEmpty() && !input[1].isEmpty() && !input[2].isEmpty()) { //planck's constant
 					double newAmount = input[2].getAmount() / input[1].getAmount();
 					input[6].setAmount(newAmount);
-					steps.add(new JLabel(input[2].getAmount() + "J / " + input[1].getAmount() + "Hz = " + newAmount + "m/s"));
+					steps.add(new JLabel("E = h\u22C5f" + input[2].getAmount() + "J / " + input[1].getAmount() + "Hz = " 
+							+ newAmount + "m/s"));
 				}
 				else if(input[6].isEmpty() && !input[0].isEmpty() && !input[3].isEmpty() && !input[4].isEmpty()) {
 					double newAmount = input[0].getAmount() * input[3].getAmount() * input[4].getAmount();
 					input[6].setAmount(newAmount);
-					steps.add(new JLabel(input[0].getAmount() + "m * " + input[3].getAmount() + "g * " + input[4].getAmount() 
-							+ "m/s = "+ newAmount + "J\u00B7s"));
+					steps.add(new JLabel("\u03BB = h/(m\u22C5v) \u2192 " + input[0].getAmount() + "m * " + input[3].getAmount() 
+							+ "g * " + input[4].getAmount() + "m/s = "+ newAmount + "J\u00B7s"));
 				}
 				else break;
 			}
 			if(!input[0].isEmpty()) {
 				double newAmount = Units.fromBaseUnit(input[0].getAmount(), input[0].getUnit());
-				steps.add(new JLabel("" + input[0].getAmount() + "m"  + " = " + newAmount + input[0].getUnitName()));
+				//steps.add(new JLabel("" + input[0].getAmount() + "m"  + " = " + newAmount + input[0].getUnitName()));
 				input[0].setAmount(newAmount);
 			}
 			if(!input[3].isEmpty()) {
 				double newAmount = Units.fromBaseUnit(input[3].getAmount(), input[3].getUnit());
-				steps.add(new JLabel("" + input[1].getAmount() + "g" + " = " + newAmount + input[1].getUnitName()));
+				//steps.add(new JLabel("" + input[1].getAmount() + "g" + " = " + newAmount + input[1].getUnitName()));
 				input[3].setAmount(newAmount);
 			}
 			steps.setVisible(true);
@@ -161,7 +167,10 @@ public class Waves extends Function {
 	}
 	
 	public String getHelp() {
-		return "Julia is the worst";
+		return "<html>Enter all known quantities, being sure to<br>"
+				+ "select the appropriate units. Click the calculate<br>"
+				+ "button and ChemHelper will find as many values<br>"
+				+ "as it can.</html>";
 	}
 	
 }
