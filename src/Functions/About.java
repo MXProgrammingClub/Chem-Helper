6c22a2b74bfee5e6348dabcfe972f6bc5116353e
giacalone99 <giacalone99@gmail.com>
2016-10-28 14:14:48 -0400

    Updating About (and dmg)

--- src/Functions/About.java
+++ src/Functions/About.java
@@ -24,19 +24,28 @@ public class About extends Function {
 		Box box = Box.createVerticalBox();
 		
 		JLabel title = new JLabel();
-		title.setFont(new Font(title.getFont().getName(), Font.BOLD, 20));
-		title.setText("ChemHelper");
+		title.setFont(new Font(title.getFont().getName(), Font.BOLD, 28));
+		title.setText("ChemHelper beta 0.5.0");
 		box.add(title);
 		
 		box.add(Box.createVerticalStrut(15));
 		
-		box.add(new JLabel("Created by Julia McClellan, Luke Giacalone, Hyun Choi, and Ted Pyne as a part of Middlesex School"
-				+ " Computer Science Club 2015-16. The program was designed"));
+		box.add(new JLabel("Created by Julia McClellan, Luke Giacalone, Ted Pyne, and Hyun Choi as a part of Middlesex School"
+				+ " Programming Club 2015-17. The program was designed"));
 		box.add(new JLabel("to help chemistry students check their work when working on homework and preparing for tests."));
 		
 		box.add(Box.createVerticalStrut(15));
 		
-		box.add(new JLabel("Copyright \u00A9 2016 MXCS Club. All rights reserved."));
+		box.add(new JLabel("<html><b>DISTRIBUTION OF THIS VERSION OF CHEMHELPER IS STRICTLY PROHIBITED!</b> Any distribution is breaking"
+				+ " a Major School Rule and will result in disciplinary action.</html>"));
+		
+		box.add(Box.createVerticalStrut(15));
+		
+		box.add(new JLabel("ChemHelper is released under the BSD 3 License."));
+		
+		box.add(Box.createVerticalStrut(15));
+		
+		box.add(new JLabel("Copyright \u00A9 2016 Programming Club. All rights reserved."));
 		
 		panel.add(box);
 	}
